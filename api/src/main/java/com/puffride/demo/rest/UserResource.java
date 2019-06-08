d144e36f04259d7c9c04eae4ab0a45a879c19b03
Jacob Menke <jamenk@email.wm.edu>
2019-06-08 19:22:14 -0400

    first api gen

---
+++ api/src/main/java/com/puffride/demo/rest/UserResource.java
@@ -0,0 +1,52 @@
+package com.puffride.demo.rest; 
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import com.puffride.demo.entity.User;
+import com.puffride.demo.dao.UserDao;
+
+import com.puffride.demo.utils.GlobalConstants;
+
+@RestController
+@RequestMapping(GlobalConstants.CONTEXT_PATH + "/user")
+public class UserResource {
+
+    @Autowired
+    UserDao dao;
+
+    @GetMapping
+    public List<User> readAll(){
+        return dao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User read(@PathVariable("id") Long id){
+        return dao.findOne(id);
+    }
+
+    @PostMapping
+    public User create(@RequestBody User entity){
+        return dao.save(entity);
+    }
+
+    @PutMapping
+    public User update(@RequestBody User entity){
+        return dao.save(entity);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable("id") Long id){
+        dao.delete(id);
+        return true;
+    }
+
+    @DeleteMapping
+    public boolean deleteAll(@RequestBody List<User> entityList){
+        dao.deleteAll(entityList);
+        return true;
+    }
+
+}
