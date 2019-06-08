e172a586903f72f3e3ff016ea08f40843a5077a4
Jacob Menke <jamenk@email.wm.edu>
2019-06-08 18:39:10 -0400

    db runner shell script

---
+++ api/db_rerun.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "drop database puffride" | mysql
+echo "create database puffride" | mysql
+
+cat /Users/wizard/forkedRepos/puffride/api/puffride_ddl.sql | mysql
+
+echo "show tables from puffride" | mysql
+
+#mysqldump puffride
