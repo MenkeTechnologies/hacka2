dcef988d366a3edcc5906936f3c1eace54281b99
MenkeTechnologies <linux.dev25@gmail.com>
2019-06-09 02:40:57 -0400

    basic app bar

--- puff_ride/src/App.js
+++ puff_ride/src/App.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 import './App.css';
 import {SignUp} from './SignUp'
 import {Login} from './Login'
-import { simpleAction } from './actions/simpleAction'
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import MenuIcon from '@material-ui/icons/Menu';
+import {makeStyles} from '@material-ui/core/styles';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCheckCircle} from "@fortawesome/free-solid-svg-icons";
+
 
 /* 
  * mapDispatchToProps
@@ -27,41 +36,110 @@ const mapDispatchToProps = (dispatch) => ({
  * mapStateToProps
 */
 const mapStateToProps = (state) => {
-  return{
-      simpleReducer: state.simpleReducer
-  }
+    return {
+        simpleReducer: state.simpleReducer
+    }
 };
 
-const LoginWithState = connect(mapStateToProps, mapDispatchToProps)(({emailOnChange, passOnChange, loginDispatch, simpleReducer})=>{
+const LoginWithState = connect(mapStateToProps, mapDispatchToProps)(({emailOnChange, passOnChange, loginDispatch, simpleReducer}) => {
     console.log(simpleReducer);
     console.log(passOnChange);
-    return <Login state={simpleReducer} emailAction={emailOnChange} passAction={passOnChange} loginAction={loginDispatch}/>
+    return <Login state={simpleReducer} emailAction={emailOnChange} passAction={passOnChange}
+                  loginAction={loginDispatch}/>
 })
 
-const SignUpWithState = connect(mapStateToProps, mapDispatchToProps)(({signUpDispatch})=>{
-  console.log("sign up action invoked")
-  return <SignUp signUpAction={signUpDispatch}/>
+const SignUpWithState = connect(mapStateToProps, mapDispatchToProps)(({signUpDispatch}) => {
+    console.log("sign up action invoked")
+    return <SignUp signUpAction={signUpDispatch}/>
 })
+
+const useStyles = makeStyles(theme => ({
+    '@global': {
+        body: {
+            backgroundColor: theme.palette.common.white,
+        },
+    },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+
 /**
  * @class App
  * @extends {Component}
  */
-class App extends Component {
-  /**
-   * @memberof App
-   * @summary handles button click 
-   */
-  simpleAction = (event) => {
-    this.props.simpleAction();
-  }
 
-  render() {
-      return<Router>
-          <Route path = "/SignUp" component={SignUpWithState}/>
-          <Route path = "/Login" component={LoginWithState}/>
-          <Route exact path = "/" component={LoginWithState}/>
-      </Router>
-  }
+class App extends Component {
+    /**
+     * @memberof App
+     * @summary handles button click
+     */
+    simpleAction = (event) => {
+        this.props.simpleAction();
+    }
+
+    render() {
+
+        let marginStyle = {
+            margin: "10px",
+            textDecoration: "none"
+        };
+        return <div>
+            <Router>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" color="inherit" aria-label="Menu">
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6">
+                            PuffRide
+                        </Typography>
+                        <Link to="/Login" variant="body2"
+                              style={marginStyle}
+                        >
+                            <Button
+                                type="button"
+                                variant="contained"
+                                color="secondary"
+                            >
+                                Login
+                            </Button>
+                        </Link>
+                        <Link to="/SignUp" variant="body2"
+                              style={marginStyle}
+                        >
+                            <Button
+                                type="button"
+                                variant="contained"
+                                color="secondary"
+                            >
+                                Signup
+                                <FontAwesomeIcon icon={faCheckCircle}/>
+                            </Button>
+                        </Link>
+                    </Toolbar>
+                </AppBar>
+                <Route path="/SignUp" component={SignUpWithState}/>
+                <Route path="/Login" component={LoginWithState}/>
+                <Route exact path="/" component={LoginWithState}/>
+            </Router>
+        </div>
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
