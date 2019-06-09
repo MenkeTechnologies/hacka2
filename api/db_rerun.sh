0bc975852c96cb37c6eebecf239552a31b3949c6
Jacob Menke <linux.dev25@gmail.com>
2019-06-09 04:01:56 -0400

    fix inserts.sql

--- api/db_rerun.sh
+++ api/db_rerun.sh
@@ -5,6 +5,8 @@ echo "create database puffride" | mysql
 
 cat /Users/wizard/forkedRepos/puffride/api/puffride_ddl.sql | mysql
 
+cat /Users/wizard/forkedRepos/puffride/api/inserts.sql | mysql
+
 echo "show tables from puffride" | mysql
 
 #mysqldump puffride
