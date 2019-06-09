f975ac3e3cd1442fa260272bfb2329241649d63e
MenkeTechnologies <linux.dev25@gmail.com>
2019-06-09 07:02:48 -0400

    pw hashing

--- api/src/main/java/com/puffride/demo/rest/UserResource.java
+++ api/src/main/java/com/puffride/demo/rest/UserResource.java
@@ -4,6 +4,8 @@ import com.puffride.demo.entity.Schedule;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,6 +21,35 @@ public class UserResource {
     @Autowired
     UserDao dao;
 
+    private String bytesToHex(byte[] hash) {
+        StringBuffer hexString = new StringBuffer();
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if(hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    @PostMapping("authenticate")
+    public Boolean findByEmailPassword(@RequestBody AuthObj authObj){
+        for (User user : dao.findAll()) {
+            
+            if (user.getEmail().equalsIgnoreCase(authObj.getEmail())){
+                try {
+                    String hashedPwd = bytesToHex(MessageDigest.getInstance("SHA-256").digest(authObj.getPassword().getBytes()));
+                    if (hashedPwd.equalsIgnoreCase(user.getPwHash())){
+                       return true;
+                    }
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return false;
+                
+    }
     @PostMapping("findByEmail")
     public List<User> findByEmail(@RequestBody AuthObj authObj){
         List<User> users = dao.findAll().stream().filter(user -> user.getEmail().equalsIgnoreCase(authObj.getEmail())).collect(Collectors.toList());
