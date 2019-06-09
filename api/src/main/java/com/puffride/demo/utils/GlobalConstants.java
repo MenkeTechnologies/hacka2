7b946ece8c07b7fc1bf0ac2f1b22c2b18dddf4c8
MenkeTechnologies <linux.dev25@gmail.com>
2019-06-08 20:18:00 -0400

    test user

--- api/src/main/java/com/puffride/demo/utils/GlobalConstants.java
+++ api/src/main/java/com/puffride/demo/utils/GlobalConstants.java
@@ -1,5 +1,5 @@
 package com.puffride.demo.utils;
 
 public class GlobalConstants {
-    public static final String CONTEXT_PATH = "/api/v1";
+    public static final String CONTEXT_PATH = "/puffride/api/v1";
 }
