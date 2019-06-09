d456e65b275e2dc244de94e5da1c1357da284036
MenkeTechnologies <linux.dev25@gmail.com>
2019-06-09 06:39:19 -0400

    add user id column

--- api/src/main/java/com/puffride/demo/rest/ScheduleResource.java
+++ api/src/main/java/com/puffride/demo/rest/ScheduleResource.java
@@ -1,9 +1,14 @@
 package com.puffride.demo.rest; 
 
+import com.puffride.demo.dao.RideDao;
+import com.puffride.demo.dao.RiderDao;
+import com.puffride.demo.entity.Ride;
+import com.puffride.demo.entity.User;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.puffride.demo.entity.Schedule;
 import com.puffride.demo.dao.ScheduleDao;
@@ -16,6 +21,23 @@ public class ScheduleResource {
 
     @Autowired
     ScheduleDao dao;
+    @Autowired
+    RideDao riderDao;
+
+    @PostMapping("findRidesByEmail")
+    public List<Ride> findRidesByEmail(@RequestBody EmailObj emailObj) {
+        List<Ride> rides = riderDao.findAll().stream().filter(r -> r.getRider().getUser().getEmail().equalsIgnoreCase(emailObj.getEmail())).collect(Collectors.toList());
+        return rides;
+    }
+
+    @PostMapping("findSchedulesByEmail")
+    public List<Schedule> findSchedulesByEmail(@RequestBody EmailObj emailObj){
+        List<Ride> rides = riderDao.findAll().stream().filter(r -> r.getRider().getUser().getEmail().equalsIgnoreCase(emailObj.getEmail())).collect(Collectors.toList());
+
+        List<Schedule> schedules = rides.stream().map(Ride::getSchedule).collect(Collectors.toList());
+
+        return schedules;
+    }
 
     @GetMapping
     public List<Schedule> readAll(){
