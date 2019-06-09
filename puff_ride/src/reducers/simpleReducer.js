a6262f7577913d73840487c3269c922ebef46a17
Nina Fan <nfan@ITHAA1NFAN1.local>
2019-06-09 00:03:50 -0400

    login is connected to simpleReducer

--- puff_ride/src/reducers/simpleReducer.js
+++ puff_ride/src/reducers/simpleReducer.js
@@ -3,14 +3,24 @@
 */
 export default (state = {}, action) => {
   switch (action.type) {
+    case 'EMAIL_UPDATE':
+      return{
+        ...state,
+        email: action.email
+      };
+    case 'PASS_UPDATE':
+      return{
+        ...state,
+        password: action.pass
+      }
     case 'SIMPLE_ACTION':
       return {
         result: action.payload
-      }
+      };
     default:
       return {
-        status: "not logged in"
-        email: ''
+        status: "not logged in",
+        email: '',
         password: ''
       }
 
