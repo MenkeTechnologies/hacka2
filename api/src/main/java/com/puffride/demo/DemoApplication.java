13f0ad92c10c51655c9fad95b916732e7cee0dd3
Jacob Menke <linux.dev25@gmail.com>
2019-06-09 02:52:50 -0400

    allow all cors

--- api/src/main/java/com/puffride/demo/DemoApplication.java
+++ api/src/main/java/com/puffride/demo/DemoApplication.java
@@ -2,10 +2,12 @@ package com.puffride.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@CrossOrigin("/**")
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
