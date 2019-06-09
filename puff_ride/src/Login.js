22727e63ac42c60832b2df406fbeb6389e80676d
Jacob Menke <linux.dev25@gmail.com>
2019-06-09 01:37:09 -0400

    fix import

--- puff_ride/src/Login.js
+++ puff_ride/src/Login.js
@@ -3,13 +3,13 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import ImageUploader from 'react-images-upload';
+import {Link} from "react-router-dom";
 
 function MadeWithLove() {
   return (
@@ -97,7 +97,7 @@ export const Login = ({state, emailAction, passAction, loginAction}) => {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="/SignUp" variant="body2">
+              <Link to="/SignUp" variant="body2">
                 Don't have an account? Sign up
               </Link>
             </Grid>
